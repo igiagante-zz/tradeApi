6a9fd320421038e34775fbd44922645f9a91005f
Ignacio Giagante <ignaciogiagante@Ignacios-MacBook-Pro.local>
2018-06-08 20:48:28 -0300

    fix lint path folder

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ const app = require('./config/express');
 const mongo = require('./config/mongo');
 const settings = require('./config/settings');
 
-if(settings.env !== 'test') {
-
+if (settings.env !== 'test') {
   mongo.connect().then((result) => {
-    
-    console.log(result);
-  
+    console.info(result); // eslint-disable-line no-console
+
     app.listen(settings.port, () => {
       console.info(`server started on port ${settings.port} (${settings.env})`); // eslint-disable-line no-console
     });
-
-  }).catch(error => console.log(` Something was wrong:  ${error}`));
+  }).catch(error => console.log(` Something was wrong:  ${error}`)); // eslint-disable-line no-console
 }
 
 module.exports = app;
